26de96f83416b3bdc00ca57f2f0263b7efa28f55
luojiyin <luojiyin@hotmail.com>
2018-07-08 22:34:54 +0800

    feat: struct

---
+++ struct1.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type Skills []string
+
+type Human struct {
+	name   string
+	age    int
+	weight int
+}
+
+type Student struct {
+	Human
+	Skills
+	int
+	speciality string
+}
+
+func main() {
+	jane := Student{Human: Human{"Jane", 35, 100}, speciality: "Biology"}
+	//现在我们来访问相应的字段
+	fmt.Println("Her name is ", jane.name)
+	fmt.Println("Her age is ", jane.age)
+	fmt.Println("Her weight is ", jane.weight)
+	fmt.Println("Her spciality is ", jane.speciality)
+
+	// 修改skill
+	jane.Skills = []string{"anatomy"}
+	fmt.Println("Her skills are", jane.Skills)
+	fmt.Println("She acquired two new ones")
+	jane.Skills = append(jane.Skills, "physics", "golang")
+	fmt.Println("Her skills now are", jane.Skills)
+
+	jane.int = 3
+	fmt.Println("Her preferred number is ", jane.int)
+}
