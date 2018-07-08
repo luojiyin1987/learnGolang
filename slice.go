387df978180cb6515453281ecd53d36e4182d592
luojiyin <luojiyin@hotmail.com>
2018-07-08 10:03:26 +0800

    feat: call function

---
+++ slice.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	x := 3
+	y := 4
+	z := 5
+
+	max_xy := max(x, y)
+	max_xz := max(x, z)
+
+	fmt.Printf("max(%d, %d) = %d\n", x, y, max_xy)
+	fmt.Printf("max(%d, %d) = %d\n", x, z, max_xz)
+	fmt.Printf("max(%d, %d) = %d\n", y, z, max(y, z))
+}
