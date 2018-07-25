03a6ca31788fc75899f6bbeff77e313995fb703f
luojiyin <luojiyin@hotmail.com>
2018-07-25 21:42:03 +0800

    feat: gorountine

---
+++ goroutine1.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("In main()")
+	go longWait()
+	go shortWait()
+	fmt.Println("About to sleep in main()")
+	time.Sleep(10 * 1e9)
+	fmt.Println("At the end of main()")
+}
+
+func longWait() {
+	fmt.Println("Beginning longWait()")
+	time.Sleep(5 * 1e9)
+	fmt.Println("End of the longWait()")
+
+}
+
+func shortWait() {
+	fmt.Println("Beginning shortWait()")
+	time.Sleep(2 * 1e9)
+	fmt.Println("End of shortWait()")
+}
